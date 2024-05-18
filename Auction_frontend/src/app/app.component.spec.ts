8b7c8e2cccbf7816f70573e95ea6601eba1a053c
Shrey <shreypatel5676@gmail.com>
2024-05-18 00:04:46 -0300

    Updated frontend folder structure

--- Auction_frontend/src/app/app.component.spec.ts
+++ Auction_frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Auction_frontend'`, () => {
+  it(`should have as title 'Online_Auction_frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Auction_frontend');
+    expect(app.title).toEqual('Online_Auction_frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Auction_frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Online_Auction_frontend');
   });
 });
--- Auction_frontend/src/app/app.component.ts
+++ Auction_frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Auction_frontend';
+  title = 'Online_Auction_frontend';
 }
