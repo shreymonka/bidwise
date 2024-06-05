295ead85eb3461e69946608ea57cc5fb0c86e4b6
shrey <sh644024@dal.ca>
2024-06-05 16:28:08 -0300

    Services module added in frontend

--- Auction_frontend/src/app/core/core.module.ts
+++ Auction_frontend/src/app/core/core.module.ts
@@ -4,12 +4,14 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { SignupPageComponent } from './components/signup-page/signup-page.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     LoginPageComponent, 
     SignupPageComponent,
-    LandingPageComponent],
+    LandingPageComponent,
+    ServicesComponent],
   exports : [],
   imports: [
     CommonModule,
