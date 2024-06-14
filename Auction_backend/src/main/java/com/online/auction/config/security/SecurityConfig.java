40bf198c1d4beda956cb57058b8974c5e4003dbe
Harshil Makwana <harshil.makwana@dal.ca>
2024-06-14 17:32:12 -0300

    Added the SecurityConfig for the Spring security and configured the JwtAuthenticationFilter for JWT token verification

--- Auction_backend/src/main/java/com/online/auction/config/security/SecurityConfig.java
+++ Auction_backend/src/main/java/com/online/auction/config/security/SecurityConfig.java
@@ -1,4 +1,52 @@
 package com.online.auction.config.security;
 
+import com.online.auction.filter.JwtAuthenticationFilter;
+import com.online.auction.handler.CustomAccessDeniedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.online.auction.model.Role.ADMIN;
+import static com.online.auction.model.Role.USER;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
+    private static final String[] WHITE_LIST_URL = {"/api/v1/user/register", "/api/v1/user/authenticate", "/api/v1/user/refresh-token"};
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll()
+                                .requestMatchers("/api/v1/user/**").hasAnyRole(ADMIN.name(), USER.name())
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex -> ex.accessDeniedHandler(new CustomAccessDeniedException()))
+                .logout(logout ->
+                        logout.logoutUrl("/api/v1/user/logout")
+                                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+                )
+        ;
+        return http.build();
+    }
 }
