34d5f43d3f8a3f1005f390277fa0b34b80df4253
Harshil Makwana <harshil.makwana@dal.ca>
2024-06-14 17:00:14 -0300

    Added the custom ServiceException

--- Auction_backend/src/main/java/com/online/auction/exception/ServiceException.java
+++ Auction_backend/src/main/java/com/online/auction/exception/ServiceException.java
@@ -1,4 +1,21 @@
 package com.online.auction.exception;
 
-public class ServiceException {
+import org.springframework.http.HttpStatus;
+
+public class ServiceException extends Exception {
+    private final int statusCode;
+    private final String errorMessage;
+
+    public ServiceException(HttpStatus statusCode, String message) {
+        this.statusCode = statusCode.value();
+        this.errorMessage = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
