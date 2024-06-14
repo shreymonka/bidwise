918092560eea1a0195937507132339e473453a2b
Harshil Makwana <harshil.makwana@dal.ca>
2024-06-14 17:13:07 -0300

    Created the interface UserService and JwtService for the User Authentication and Token generation

--- Auction_backend/src/main/java/com/online/auction/service/UserService.java
+++ Auction_backend/src/main/java/com/online/auction/service/UserService.java
@@ -1,4 +1,18 @@
 package com.online.auction.service;
 
+import com.online.auction.dto.AuthenticationRequestDTO;
+import com.online.auction.dto.AuthenticationResponseDTO;
+import com.online.auction.dto.UserDTO;
+import com.online.auction.exception.ServiceException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 public interface UserService {
+    AuthenticationResponseDTO register(UserDTO userDto);
+
+    AuthenticationResponseDTO authenticate(AuthenticationRequestDTO authenticationRequest) throws ServiceException;
+
+    AuthenticationResponseDTO refreshToken(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException;
 }
