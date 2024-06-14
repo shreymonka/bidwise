ff48aa97ee0ce401fa8c0b3f3fdc61da95e3b158
Harshil Makwana <harshil.makwana@dal.ca>
2024-06-14 17:02:02 -0300

    Added the required DTOs for the User Registration and Authentication

--- Auction_backend/src/main/java/com/online/auction/dto/AuthenticationRequestDTO.java
+++ Auction_backend/src/main/java/com/online/auction/dto/AuthenticationRequestDTO.java
@@ -1,6 +1,5 @@
 package com.online.auction.dto;
 
-import com.online.auction.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,13 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDTO {
-    private String firstname;
-    private String lastname;
+public class AuthenticationRequestDTO {
     private String email;
     private String password;
-    private Role role;
-    private String city;
-    private String timezone;
-    private boolean isPremium;
 }
