48d2b84b138faf07f57448ac1307503f1089f535
Harshil Makwana <harshil.makwana@dal.ca>
2024-06-14 16:59:10 -0300

    Created the AuctionConstants for static constants

---
+++ Auction_backend/src/main/java/com/online/auction/constant/AuctionConstants.java
@@ -0,0 +1,8 @@
+package com.online.auction.constant;
+
+public class AuctionConstants {
+    public static final String TIMESTAMP = "timestamp";
+    public static final String HTTP_CODE = "httpCode";
+    public static final String HTTP_STATUS = "httpStatus";
+    public static final String ERROR_MSG = "errorMessage";
+}
