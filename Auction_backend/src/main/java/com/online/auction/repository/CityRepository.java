4ed6e3eb8eeb155f398f284ae9a09359b0277098
Harshil Makwana <harshil.makwana@dal.ca>
2024-06-15 13:36:26 -0300

    Created the CityRepository interface along with method to fetch the city based on the name

---
+++ Auction_backend/src/main/java/com/online/auction/repository/CityRepository.java
@@ -0,0 +1,10 @@
+package com.online.auction.repository;
+
+import com.online.auction.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CityRepository extends JpaRepository<City, Integer> {
+    Optional<City> findByCityName(String cityName);
+}
