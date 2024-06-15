87d8e49d79b4ee80d03bfe9e58ab230b1adae546
Harshil Makwana <harshil.makwana@dal.ca>
2024-06-15 00:51:25 -0300

    Added the constants in AuctionConstants

--- Auction_backend/src/main/java/com/online/auction/constant/AuctionConstants.java
+++ Auction_backend/src/main/java/com/online/auction/constant/AuctionConstants.java
@@ -5,4 +5,20 @@ public class AuctionConstants {
     public static final String HTTP_CODE = "httpCode";
     public static final String HTTP_STATUS = "httpStatus";
     public static final String ERROR_MSG = "errorMessage";
+
+    public static final String USER_ALREADY_PRESENT_MSG = "User Already Exists!";
+
+    public static final String UNAUTHORIZED = "UnAuthorized";
+
+    public static final String JWT_EXPIRED_MSG = "JWT token is expired";
+
+    public static final int UNAUTHORIZED_STATUS_CODE = 401;
+
+    public static final String APPLICATION_JSON = "application/json";
+
+    public static final int INTEGER_SEVEN = 7;
+
+    public static final String BEARER = "Bearer ";
+
+    public static final String AUTHORIZATION = "Authorization";
 }
