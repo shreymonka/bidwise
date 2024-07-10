83c98b600cd8ce17a1a45911e7e52f7144b04eda
Shrey Devendrabhai Monka <sh605539@dal.ca>
2024-07-10 10:34:12 -0300

    Made 3 components for item listing and categories. And 1 component in shared module for body header.

---
+++ Auction_frontend/src/app/core/components/item-listing/item-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-listing',
+  templateUrl: './item-listing.component.html',
+  styleUrl: './item-listing.component.css'
+})
+export class ItemListingComponent {
+
+}
