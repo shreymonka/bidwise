3eee95a95f69cd87118e73c1aef7ab52a1d70fa4
Harshil Makwana <harshil.makwana@dal.ca>
2024-07-15 10:03:24 -0300

    Updated the WebSocketConfig by making the allowedOrigins configurable

--- Auction_backend/src/main/java/com/online/auction/config/application/WebSocketConfig.java
+++ Auction_backend/src/main/java/com/online/auction/config/application/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.online.auction.config.application;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${cors.allowed.origins}")
+    private String allowedOrigins;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -17,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://localhost:4200");
-        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://localhost:4200").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins(allowedOrigins);
+        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins(allowedOrigins).withSockJS();
     }
 }
