6bfdc240e146d998758e31360d0dd70669ed8e55
Harshil Makwana <harshil.makwana@dal.ca>
2024-07-15 09:41:44 -0300

    Added the method findByItem_ItemId in the AuctionListingRepository

--- Auction_backend/src/main/java/com/online/auction/repository/AuctionListingRepository.java
+++ Auction_backend/src/main/java/com/online/auction/repository/AuctionListingRepository.java
@@ -1,11 +1,10 @@
 package com.online.auction.repository;
 
 import com.online.auction.model.Auction;
-import com.online.auction.model.ItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface AuctionListingRepository extends JpaRepository<Auction,Integer> {
-
+public interface AuctionListingRepository extends JpaRepository<Auction, Integer> {
+    Optional<Auction> findByItems_ItemId(int itemId);
 }
