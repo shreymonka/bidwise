53a8c0b31e3097496471ccfbedcb5e76ad08b18b
Harshil Makwana <harshil.makwana@dal.ca>
2024-07-16 00:03:02 -0300

    Added the test cases for the AuctionServiceImpl

--- Auction_backend/src/test/java/com/online/auction/service/impl/AuctionServiceImplTest.java
+++ Auction_backend/src/test/java/com/online/auction/service/impl/AuctionServiceImplTest.java
@@ -2,12 +2,22 @@ package com.online.auction.service.impl;
 
 
 import com.online.auction.exception.ServiceException;
+import com.online.auction.model.Auction;
 import com.online.auction.repository.AuctionListingRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+
+import static com.online.auction.constant.AuctionConstants.AUCTION_NOT_FOUND_MSG;
+import static com.online.auction.constant.TestConstants.INTEGER_ONE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 class AuctionServiceImplTest {
     @Mock
@@ -23,11 +33,23 @@ class AuctionServiceImplTest {
 
     @Test
     void getAuctionDetailsTest() throws ServiceException {
+        Auction auction = new Auction();
+        when(auctionListingRepository.findByItems_ItemId(INTEGER_ONE)).thenReturn(Optional.of(auction));
 
+        Auction result = auctionService.getAuctionDetails(INTEGER_ONE);
+
+        assertEquals(auction, result);
     }
 
     @Test
     void getAuctionDetailsWhenAuctionNotPresentTest() {
+        when(auctionListingRepository.findByItems_ItemId(INTEGER_ONE)).thenReturn(Optional.empty());
 
+        ServiceException exception = assertThrows(ServiceException.class, () -> {
+            auctionService.getAuctionDetails(INTEGER_ONE);
+        });
+
+        assertEquals(HttpStatus.BAD_REQUEST.value(), exception.getStatusCode());
+        assertEquals(AUCTION_NOT_FOUND_MSG, exception.getErrorMessage());
     }
 }
