2f1f8242f48fba6e31182d8d012f147703284757
Shrey Devendrabhai Monka <sh605539@dal.ca>
2024-07-17 14:29:53 -0300

    Made the add funds component and added the HTML skeleton for it.

---
+++ Auction_frontend/src/app/core/components/add-funds/add-funds.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-funds',
+  templateUrl: './add-funds.component.html',
+  styleUrl: './add-funds.component.css'
+})
+export class AddFundsComponent {
+
+}
