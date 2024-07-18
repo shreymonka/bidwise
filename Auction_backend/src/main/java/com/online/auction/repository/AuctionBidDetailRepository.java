a2b9dcb6aba1932469c953893ee8757559b1b8f9
Harshil Makwana <harshil.makwana@dal.ca>
2024-07-18 02:24:24 -0300

    Added the method findTopByItemIdOrderByBidAmountDesc in AuctionBidDetailRepository

--- Auction_backend/src/main/java/com/online/auction/repository/AuctionBidDetailRepository.java
+++ Auction_backend/src/main/java/com/online/auction/repository/AuctionBidDetailRepository.java
@@ -2,6 +2,13 @@ package com.online.auction.repository;
 
 import com.online.auction.model.AuctionBidDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface AuctionBidDetailRepository extends JpaRepository<AuctionBidDetails, Integer> {
+    @Query(value = "SELECT * FROM auction_bid_detail WHERE item_id = :itemId ORDER BY bid_amount DESC LIMIT 1", nativeQuery = true)
+    AuctionBidDetails findTopByItemIdOrderByBidAmountDesc(@Param("itemId") int itemId);
 }
