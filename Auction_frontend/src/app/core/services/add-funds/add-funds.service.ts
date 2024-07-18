e0a26f4d40f0d09a5a910633205f4989b2341a1d
Shrey Devendrabhai Monka <sh605539@dal.ca>
2024-07-18 12:26:01 -0300

    Added the services for the add funds in the add .service.ts file

--- Auction_frontend/src/app/core/services/add-funds/add-funds.service.ts
+++ Auction_frontend/src/app/core/services/add-funds/add-funds.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddFundsService {
+  endpoint: string = 'http://localhost:8080/api/v1/account/addFunds';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  addFunds(userId: number, amount: number): Observable<any> {
+    const url = `${this.endpoint}/add-funds`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+
+    const body = new URLSearchParams();
+    body.set('userId', userId.toString());
+    body.set('amount', amount.toString());
+
+    return this.http.post(url, body.toString(), { headers }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = `Client-side error: ${error.error.message}`;
+    } else {
+      // Server-side error
+      errorMessage = `Server-side error: ${error.status} ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
 }
