508deb4bbe9c1c10aa0202b5b8ce63c9696ad5fe
Shrey Devendrabhai Monka <sh605539@dal.ca>
2024-07-17 21:46:08 -0300

    Made an account service as well as the implementation of the service for handling account related stuff.

---
+++ Auction_backend/src/main/java/com/online/auction/service/impl/AccountServiceImpl.java
@@ -0,0 +1,12 @@
+package com.online.auction.service.impl;
+
+import com.online.auction.exception.ServiceException;
+import com.online.auction.service.AccountService;
+
+public class AccountServiceImpl implements AccountService {
+
+    @Override
+    public float getAccountBalance(Integer userId) throws ServiceException {
+        return 0;
+    }
+}
