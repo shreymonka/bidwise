7f2ce1dcb8e3a2801ca9f0fe19ffda308a1db986
Shrey Devendrabhai Monka <sh605539@dal.ca>
2024-07-20 19:31:43 -0300

    Wrote the ts file for the user-profile service

--- Auction_frontend/src/app/core/services/user-profile/user-profile.service.ts
+++ Auction_frontend/src/app/core/services/user-profile/user-profile.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserProfileService {
 
-  constructor() { }
+  private endPoint = 'http://localhost:8080/api/v1/user/profile';
+
+  constructor(private http: HttpClient) { }
+
+  // Method to fetch user profile data
+  getUserProfile(): Observable<any> {
+    return this.http.get<any>(`${this.endPoint}`);
+  }
+
+  // Method to fetch chart data
+  getChartData(): Observable<any> {
+    return this.http.get<any>(`${this.endPoint}`);
+  }
 }
