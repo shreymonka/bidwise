288cf11e0b0baf9156d0dcc048d598ab6b760f10
Shrey Devendrabhai Monka <sh605539@dal.ca>
2024-07-22 17:00:17 -0300

    Changed the @RequestParam to @AuthenticationPrincipal.

--- Auction_backend/src/main/java/com/online/auction/controller/ProfileController.java
+++ Auction_backend/src/main/java/com/online/auction/controller/ProfileController.java
@@ -5,11 +5,13 @@ import com.online.auction.dto.CategoryBidStatsDTO;
 import com.online.auction.dto.SuccessResponse;
 import com.online.auction.dto.UserProfileDTO;
 import com.online.auction.exception.ServiceException;
+import com.online.auction.model.User;
 import com.online.auction.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,32 +32,32 @@ public class ProfileController {
 
     @GetMapping("/details")
     public ResponseEntity<SuccessResponse<UserProfileDTO>> getUserProfile(
-            @RequestParam("userId") Integer userId) throws ServiceException {
-        UserProfileDTO userProfile = profileService.getUserProfile(userId);
+            @AuthenticationPrincipal User user) throws ServiceException {
+        UserProfileDTO userProfile = profileService.getUserProfile(user.getUserId());
         SuccessResponse<UserProfileDTO> response = new SuccessResponse<>(200, HttpStatus.OK, userProfile);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/auctionsParticipated")
     public ResponseEntity<SuccessResponse<Long>> countUserParticipatedAuctions(
-            @RequestParam("userId") Integer userId) throws ServiceException {
-        long auctionCount = profileService.countUserParticipatedAuctions(userId);
+            @AuthenticationPrincipal User user) throws ServiceException {
+        long auctionCount = profileService.countUserParticipatedAuctions(user.getUserId());
         SuccessResponse<Long> response = new SuccessResponse<>(200, HttpStatus.OK, auctionCount);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/bidStats")
     public ResponseEntity<SuccessResponse<List<BidStatsDTO>>> getBidStats(
-            @RequestParam("userId") Integer userId) throws ServiceException {
-        List<BidStatsDTO> bidStats = profileService.getBidStats(userId);
+            @AuthenticationPrincipal User user) throws ServiceException {
+        List<BidStatsDTO> bidStats = profileService.getBidStats(user.getUserId());
         SuccessResponse<List<BidStatsDTO>> response = new SuccessResponse<>(200, HttpStatus.OK, bidStats);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/categoryBidStats")
     public ResponseEntity<SuccessResponse<List<CategoryBidStatsDTO>>> getCategoryBidStats(
-            @RequestParam("userId") Integer userId) throws ServiceException {
-        List<CategoryBidStatsDTO> categoryBidStats = profileService.getCategoryBidStats(userId);
+            @AuthenticationPrincipal User user) throws ServiceException {
+        List<CategoryBidStatsDTO> categoryBidStats = profileService.getCategoryBidStats(user.getUserId());
         SuccessResponse<List<CategoryBidStatsDTO>> response = new SuccessResponse<>(200, HttpStatus.OK, categoryBidStats);
         return ResponseEntity.ok(response);
     }
