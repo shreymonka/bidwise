9227dfcd0727bbd5659043ef291f8a95ae477723
Harshil Makwana <harshil.makwana@dal.ca>
2024-07-22 13:16:50 -0300

    Updated the AuctionServiceImpl with the logic to update the account, item table after the auction along with adding the loggers

--- Auction_backend/src/main/java/com/online/auction/service/impl/AuctionServiceImpl.java
+++ Auction_backend/src/main/java/com/online/auction/service/impl/AuctionServiceImpl.java
@@ -2,12 +2,17 @@ package com.online.auction.service.impl;
 
 import com.online.auction.dto.AuctionDTO;
 import com.online.auction.exception.ServiceException;
+import com.online.auction.model.Account;
 import com.online.auction.model.Auction;
 import com.online.auction.model.AuctionBidDetails;
+import com.online.auction.model.Item;
+import com.online.auction.repository.AccountRepository;
 import com.online.auction.repository.AuctionBidDetailRepository;
 import com.online.auction.repository.AuctionListingRepository;
+import com.online.auction.repository.ItemRepository;
 import com.online.auction.service.AuctionService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -18,19 +23,23 @@ import static com.online.auction.constant.AuctionConstants.AUCTION_NOT_FOUND_MSG
 
 @AllArgsConstructor
 @Service
+@Slf4j
 public class AuctionServiceImpl implements AuctionService {
 
     private AuctionListingRepository auctionListingRepository;
     private AuctionBidDetailRepository auctionBidDetailRepository;
+    private AccountRepository accountRepository;
+    private ItemRepository itemRepository;
 
     @Override
     public AuctionDTO getAuctionDetails(int itemId) throws ServiceException {
+        log.info("Fetching the Auction Details for the itemId: {}", itemId);
         Optional<Auction> auctionOptional = auctionListingRepository.findByItems_ItemId(itemId);
         if (auctionOptional.isEmpty()) {
             throw new ServiceException(HttpStatus.BAD_REQUEST, AUCTION_NOT_FOUND_MSG);
         }
         Auction auctionDb = auctionOptional.get();
-
+        log.info("The Auction details in Db are: {}", auctionDb);
         return AuctionDTO.builder()
                 .auctionId(String.valueOf(auctionDb.getAuctionId()))
                 .isOpen(auctionDb.isOpen())
@@ -43,18 +52,41 @@ public class AuctionServiceImpl implements AuctionService {
 
     @Override
     public boolean processPostAuctionState(int itemId) throws ServiceException {
+        log.info("Started the auction Postprocessing");
         AuctionBidDetails auctionBidDetails = auctionBidDetailRepository.findTopByItemIdOrderByBidAmountDesc(itemId);
         if (Objects.isNull(auctionBidDetails)) {
             throw new ServiceException(HttpStatus.BAD_REQUEST, "Record not found to update post Auction state");
         }
+        log.info("The winning bid information is : {}", auctionBidDetails);
         auctionBidDetails.setWon(true);
         auctionBidDetailRepository.save(auctionBidDetails);
+        log.info("Successfully updated the winner in Auction Bid Details : {}", auctionBidDetails);
+
+        Optional<Item> itemOptional = itemRepository.findById(itemId);
+        if (!itemOptional.isEmpty()) {
+            log.info("Updating the selling amount for the item: {}", itemOptional.get());
+            Item item = itemOptional.get();
+            item.setSelling_amount(auctionBidDetails.getBid_amount());
+            itemRepository.save(item);
+            log.info("Successfully updated the selling amount for the item: {}", item);
+        }
+
+        Account account = accountRepository.findByUserId(auctionBidDetails.getBidderId().getUserId());
+        if (Objects.nonNull(account)) {
+            log.info("Debiting the fund for the user: {}", account.getUserId());
+            double currentFunds = account.getFunds();
+            account.setFunds(currentFunds - auctionBidDetails.getBid_amount());
+            accountRepository.save(account);
+            log.info("Successfully debited the funds from the winner's account : {}", account);
+        }
 
         Optional<Auction> auctionOptional = auctionListingRepository.findByItems_ItemId(itemId);
         if (!auctionOptional.isEmpty()) {
             Auction auction = auctionOptional.get();
+            log.info("Closing the Auction: {}", auction);
             auction.setOpen(false);
             auctionListingRepository.save(auction);
+            log.info("Successfully closed the Auction: {}", auction);
         }
         return true;
     }
