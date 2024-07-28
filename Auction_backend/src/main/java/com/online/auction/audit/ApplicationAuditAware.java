7741944fe5d761ce4cc5904740c4dcf62545b4bc
Harshil Makwana <harshil.makwana@dal.ca>
2024-07-28 14:12:16 -0300

    Added the javadoc for the ApplicationAuditAware

--- Auction_backend/src/main/java/com/online/auction/audit/ApplicationAuditAware.java
+++ Auction_backend/src/main/java/com/online/auction/audit/ApplicationAuditAware.java
@@ -8,6 +8,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
+/**
+ * Provides information about the current auditor (user) for auditing purposes.
+ * <p>
+ * This implementation of {@link AuditorAware} is used to retrieve the current
+ * authenticated user's ID. It checks the security context to determine if the user
+ * is authenticated and not anonymous. If these conditions are met, it returns
+ * the user's ID as the current auditor; otherwise, it returns an empty {@link Optional}.
+ * </p>
+ * <p>
+ * This class is used to support auditing functionality by providing the ID of the
+ * user performing the action being audited, which can be useful for tracking changes
+ * and maintaining an audit trail in the application.
+ * </p>
+ */
 public class ApplicationAuditAware implements AuditorAware<Integer> {
     @Override
     public Optional<Integer> getCurrentAuditor() {
