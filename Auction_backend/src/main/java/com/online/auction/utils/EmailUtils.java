c867c0c55d7b5ba962fd33f1e6aa1bb0b1e3c895
Harshil Makwana <harshil.makwana@dal.ca>
2024-07-28 14:38:52 -0300

    Added the documentation for the EmailUtils

--- Auction_backend/src/main/java/com/online/auction/utils/EmailUtils.java
+++ Auction_backend/src/main/java/com/online/auction/utils/EmailUtils.java
@@ -7,6 +7,13 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for sending email notifications.
+ * <p>
+ * This class provides functionality to send simple email messages using Spring's {@link JavaMailSender}.
+ * It includes methods for constructing and sending email messages with specified recipients, subjects, and bodies.
+ * </p>
+ */
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -14,6 +21,17 @@ public class EmailUtils {
 
     private final JavaMailSender javaMailSender;
 
+    /**
+     * Sends an email with the given details.
+     * <p>
+     * This method constructs a {@link SimpleMailMessage} with the provided recipient email address, subject, and body,
+     * and sends it using the configured {@link JavaMailSender}.
+     * </p>
+     *
+     * @param toEmail The recipient's email address.
+     * @param subject The subject of the email.
+     * @param body    The body content of the email.
+     */
     public void sendEmail(String toEmail, String subject, String body) {
         log.info("All information for mail received");
         SimpleMailMessage message = new SimpleMailMessage();
